59eb120b06c1743c6c9c3a60b0969c56d2c6799b
Paul Annesley <paul@annesley.cc>
2014-05-02 18:21:54 -0700

    gobean --tubes=one,two,three

--- gobean.go
+++ gobean.go
@@ -1,22 +1,35 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
+	"strings"
 	"time"
 )
 import "github.com/kr/beanstalk"
 
 func main() {
-	fmt.Println("hello gobean")
+	tubes := tubesFlag()
 
 	c, err := beanstalk.Dial("tcp", "127.0.0.1:11300")
 	if err != nil {
 		panic(err)
 	}
 
+	log.Println("watching", tubes)
+	ts := beanstalk.NewTubeSet(c, tubes...)
+
 	for {
-		id, body, err := c.Reserve(24 * time.Hour)
+		id, body, err := ts.Reserve(24 * time.Hour)
 		fmt.Println(id, string(body), err)
-		c.Delete(id)
+		ts.Conn.Delete(id)
 	}
+}
+
+func tubesFlag() []string {
+	var raw string
+	flag.StringVar(&raw, "tubes", "default", "Comma-separated list of tubes to watch")
+	flag.Parse()
+	return strings.Split(raw, ",")
 }
