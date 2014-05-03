37fe31a1b6315ee109f4d9d2b99bc64f8e94fa44
Paul Annesley <paul@annesley.cc>
2014-05-02 17:57:54 -0700

    Stub code to reserve and delete tasks.

--- gobean.go
+++ gobean.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+import "github.com/kr/beanstalk"
 
 func main() {
 	fmt.Println("hello gobean")
+
+	c, err := beanstalk.Dial("tcp", "127.0.0.1:11300")
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		id, body, err := c.Reserve(24 * time.Hour)
+		fmt.Println(id, string(body), err)
+		c.Delete(id)
+	}
 }
